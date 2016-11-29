ab1fed890271b0b3b809c6f89a1713c9501234c4
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2016-11-29 18:29:50 +0900

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sc_20161129_rails_subdomain_session', domain: '.lvh.me'
+Rails.application.config.session_store :cookie_store, key: '_sc_20161129_rails_subdomain_session'
